25a512820a86f6f240dc01fdc23a6cfe39d88512
b-naveen <47445146+b-naveen@users.noreply.github.com>
2019-04-04 17:22:35 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/open?id=1dYAIOrI8liWpHql_vKFglwNHlxY7ka7Q'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1dYAIOrI8liWpHql_vKFglwNHlxY7ka7Q'
 export_file_name = 'export.pkl'
 
-classes = ['malvi', 'naveen']
+classes = ['Malvi', 'naveen']
 path = Path(__file__).parent
 
 app = Starlette()
